d4a3805615b22e6669d88e63d3cc57776ab80100
dexter-stack <muhtohasulaimon@gmail.com>
2024-07-25 10:38:04 -0700

    insertNthPosition function implemented

--- singleList/SingleLinkedList.php
+++ singleList/SingleLinkedList.php
@@ -2,7 +2,8 @@
 
 require "Node.php";
 
-class SingleLinkedList{
+class SingleLinkedList
+{
 
     public  $head;
 
@@ -11,38 +12,38 @@ class SingleLinkedList{
         $this->head = $head;
     }
 
-    public function getHead()  {
+    public function getHead()
+    {
 
         return $this->head;
-        
     }
 
-    
 
 
-    public function display(){
 
-        if($this->head == null){
+    public function display()
+    {
+
+        if ($this->head == null) {
             return null;
         }
         $currentNode = $this->head;
-        while($currentNode != null){
-           
+        while ($currentNode != null) {
+
             echo "$currentNode->data ->";
             $currentNode = $currentNode->nextNode;
-           
         }
         echo "null";
     }
 
 
 
-    public function insertFirst($data){
+    public function insertFirst($data)
+    {
 
         $newNode = new Node($data);
-        if($this->head == null){
+        if ($this->head == null) {
             $this->head  = $newNode;
-            
         }
 
         $newNode->nextNode = $this->head; // link the newNode to the head 
@@ -50,23 +51,54 @@ class SingleLinkedList{
 
     }
 
-    public function insertLast($data){
+    public function insertLast($data)
+    {
         $newNode = new Node($data);
 
-        if($this->head == null){
+        if ($this->head == null) {
             $this->head =  $newNode;
         }
         $currentNode = $this->head;
-        while($currentNode->nextNode != null){
+        while ($currentNode->nextNode != null) {
             $currentNode =  $currentNode->nextNode;
-
         }
-         $currentNode->nextNode = $newNode; // link the new node with the current node 
-         $newNode->nextNode = null;  // then link the new node before the null
+        $currentNode->nextNode = $newNode; // link the new node with the current node 
+        $newNode->nextNode = null;  // then link the new node before the null
 
     }
 
 
+    public function insertNthPosition($position, $data){
+
+        $newNode  = new Node($data);
+        if($position == 1){
+            $newNode->nextNode = $this->head;
+            $this->head = $newNode;
+        }else{
+
+            $count = 1;
+            $previousNode = $this->head;  // the previous is pointing
+            
+            while($count < $position-1){
+   
+           $previousNode = $previousNode->nextNode;
+           $count = $count+1;
+              
+   
+   
+            }
+         $currentNode = $previousNode->nextNode;  // keep the current node here 
+         $previousNode->nextNode = $newNode;   // link the new node with the previous node 
+         $newNode->nextNode = $currentNode;   // shift the new node before the current node 
+       
+   
+
+        }
+
+        
+
+
+    }
 }
 
 $head =  new Node(57);
@@ -81,32 +113,21 @@ $head->nextNode = $first;
 $first->nextNode = $second;
 $second->nextNode = $third;
 $third->nextNode = null;
+
+/*
+implementing the insertFirst($data) function
+*/
 $list->insertFirst(34);
 $list->insertFirst(67);
+
+/*
+implementing the insertLast($data) function
+*/
 $list->insertLast(46);
 $list->insertLast(68);
+
+$list->insertNthPosition(3,200);
 
 
 
 $list->display();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
